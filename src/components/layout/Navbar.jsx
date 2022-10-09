29431511c11e5bf4a6a381c80e6752019b070b76
DevTechJr <devtechjr@gmail.com>
2022-10-09 18:18:54 -0400

    added sticky to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div class="bg-primary w-full rounded-xl ">
+    <div class="bg-primary w-full rounded-xl sticky top-0 z-[100]">
       <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 rounded-xl">
         <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
           <ul class="flex items-center hidden space-x-8 lg:flex">
